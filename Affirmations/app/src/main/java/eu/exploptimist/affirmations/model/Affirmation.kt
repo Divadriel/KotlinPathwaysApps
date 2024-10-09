83450ae47ae771e7e9d546bb3b763e8a8600d85d
David Rei <rei.david26@gmail.com>
2024-10-09 16:26:53 +0200

    deleted old things

--- Affirmations/app/src/main/java/eu/exploptimist/affirmations/model/Affirmation.kt
+++
@@ -1,7 +0,0 @@
-package eu.exploptimist.affirmations.model
-
-import androidx.annotation.DrawableRes
-import androidx.annotation.StringRes
-
-data class Affirmation(@StringRes val stringResourceId: Int, @DrawableRes val imageResourceId: Int) {
-}
--- DiceRoller/README.md
+++
@@ -1,3 +0,0 @@
-# DiceRoller app
-A small project from Google Developer Team.
-Link to the tutorial: [Kotlin Basics](https://developer.android.com/courses/android-basics-kotlin/unit-1)
